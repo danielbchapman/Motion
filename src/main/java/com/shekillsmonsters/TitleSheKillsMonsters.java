a4a5ff8420f9873eda8380dcfd920bb9a98d854e
danielbchapman <chapman@danielbchapman.com>
2015-09-26 11:17:37 -0500

    - added a title layer

---
+++ src/main/java/com/shekillsmonsters/TitleSheKillsMonsters.java
@@ -0,0 +1,46 @@
+package com.shekillsmonsters;
+
+import shows.troubledwater.CharacterBrush;
+import shows.troubledwater.TWCueStack;
+
+import com.danielbchapman.layers.BleedingCanvasLayer;
+import com.danielbchapman.physics.toxiclibs.Actions;
+import com.danielbchapman.physics.toxiclibs.Layer;
+import com.danielbchapman.physics.toxiclibs.MotionEngine;
+
+public class TitleSheKillsMonsters extends BleedingCanvasLayer
+{
+
+	TWCueStack stack;
+
+	public TitleSheKillsMonsters(MotionEngine engine)
+	{
+		super(engine);
+
+		stack = new TWCueStack(this)
+		{
+			@Override
+			public void load()
+			{
+				CharacterBrush pen = new CharacterBrush();
+
+				add(load(40, 0, 533, 400, "One", "show/prologue/leaf-1", pen),
+						load(40, -25, 533, 533, "Leaf", "show/prologue/leaf-2", pen, Actions.follow(3000)));
+			}
+		};
+
+		stack.load();
+	}
+
+	@Override
+	public String getSpecificName()
+	{
+		return "Prologue";
+	}
+
+	public void go(MotionEngine engine)
+	{
+		stack.go(engine, this);
+	}
+
+}
