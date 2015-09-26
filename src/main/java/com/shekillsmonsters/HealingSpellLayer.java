da5a67f69ccf0002747e42d079c976e094a6766c
chapman@danielbchapman.com <chapman@danielbchapman.com>
2015-09-26 18:07:31 -0500

    - changed source files

--- src/main/java/com/shekillsmonsters/HealingSpellLayer.java
+++ src/main/java/com/shekillsmonsters/HealingSpellLayer.java
@@ -2,8 +2,6 @@ package com.shekillsmonsters;
 
 import java.io.File;
 
-import processing.core.PApplet;
-
 import com.danielbchapman.physics.toxiclibs.Action;
 import com.danielbchapman.physics.toxiclibs.Actions;
 import com.danielbchapman.physics.toxiclibs.Cue;
@@ -11,13 +9,17 @@ import com.danielbchapman.physics.toxiclibs.MotionEngine;
 import com.danielbchapman.physics.toxiclibs.MotionInteractiveBehavior;
 import com.danielbchapman.physics.toxiclibs.SpriteLayer;
 
+import processing.core.PApplet;
+import shows.troubledwater.TWCueStack;
+
 public class HealingSpellLayer extends SpriteLayer
 {
+	TWCueStack stack;
 
 	public HealingSpellLayer(PApplet app)
 	{
 		super(app);
-		super.image = app.loadImage("test/sprite/sprite-light.png");
+		super.image = app.loadImage("test/sprite/healing.png");
 	    quad = app.createShape();
 	    quad.beginShape();
 	    quad.noFill();
@@ -29,18 +31,29 @@ public class HealingSpellLayer extends SpriteLayer
 	    quad.vertex(50, 50, 0, image.width, image.height);
 	    quad.vertex(-50, 50, 0, 0, image.height);
 	    quad.endShape();
+	    
+	    stack = new TWCueStack(this)
+		{
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void load() {
+				add(
+					load("Load Variables", 
+						"show/healing/healing.env",
+						"show/healing/healing",
+						"show/healing/healing-brush")
+				);
+			}
+	
+		};
+		
+		stack.load();
 	}
 
 	public void go(MotionEngine engine)
 	{
-		//reset variables
-		MotionInteractiveBehavior beh = MotionInteractiveBehavior.load(new File("show/healing/healing-brush"));
-		engine.brush = beh;
-		
-		Action env = Actions.loadEnvironment(new File("show/healing/healing.env"));
-		Cue x = new Cue("Load env...", null);
-		x.addAction(env);
-		x.go(this,  engine);
+		stack.go(engine, this);
 		
 	}
 	
