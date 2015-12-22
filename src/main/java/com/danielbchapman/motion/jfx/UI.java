6b72705c8b628fe85de46e808bbfe6b820c87d3d
Daniel B. Chapman <chapman@danielbchapman.com>
2015-12-22 09:45:41 -0700

    - added new mockup for the UI framework

--- src/main/java/com/danielbchapman/motion/jfx/UI.java
+++ src/main/java/com/danielbchapman/motion/jfx/UI.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -24,15 +25,18 @@ public class UI extends Application
     VBox content = new VBox();
     
     VBox cueList = new VBox();
-    cueList.setMinWidth(200);
+    cueList.setMinWidth(300);
     cueList.getChildren().add(new Label("Cue List"));
+    
+    ScrollPane cueScroll = new ScrollPane(cueList);
+    cueScroll.setMinWidth(300);
+    cueScroll.setContent(cueList);
     
     for(int i = 1; i < 11; i++)
     {
       ContentCue cue = new ContentCue();
       cue.setLabel("Test Cue " + i);
-      cue.setCueNumber(new BigDecimal(i));
-      
+      cue.setCueNumber(new BigDecimal(i));      
       cueList.getChildren().add(cue);
     }
     
@@ -43,13 +47,15 @@ public class UI extends Application
           new Label("Editor Area"),
           new Label("Properties")
           );
-
+    
     workspace.getChildren()
       .addAll(
-          cueList,
-          content
+          cueScroll,
+          new ScrollPane(content)
           );
     
+    stage.setMinWidth(800);
+    stage.setMinHeight(600);
     stage.setScene(new Scene(workspace));
     stage.setTitle("Motion 0.0.1 - TEST");
     stage.show();
