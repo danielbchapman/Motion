18486271e14f955e62d6c423c865df2fd7bbed36
Daniel B. Chapman <chapman@danielbchapman.com>
2016-02-20 16:32:11 -0600

    Refactored the Syphon code to work via reflection for cross platform compatibility

--- src/main/java/com/danielbchapman/physics/toxiclibs/SyphonGraphicsShare.java
+++ src/main/java/com/danielbchapman/physics/toxiclibs/SyphonGraphicsShare.java
@@ -1,27 +1,76 @@
 package com.danielbchapman.physics.toxiclibs;
 
-import codeanticode.syphon.SyphonServer;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import processing.core.PApplet;
 import processing.core.PGraphics;
 
-public class SyphonGraphicsShare implements IGraphicShare{
+public class SyphonGraphicsShare implements IGraphicShare
+{
 
-	SyphonServer server;
-	@Override
-	public void initialize(PApplet app) {
-		System.out.println("SYPHON INITIALZIED");
-		server = new SyphonServer(app, "Motion Syphon Server");
-	}
+  public static String SYPHON_INSTANCE_NAME = "Motion Syphon Server";
 
-	@Override
-	public void cleanup() {
-		server.stop();
-		System.out.println("CLEANUP");
-	}
+  Object syphonServer;
+  Method stop;
+  Method sendImage;
+  Constructor<?> syphonConst;
 
-	@Override
+  @Override
+  public void initialize(PApplet app)
+  {
+    Class<?> syphonServerClass = null;
+
+    // I hate cross platform code!
+    // server = new SyphonServer(app, "Motion Syphon Server");
+    try
+    {
+      syphonServerClass = Class.forName("codeanticode.syphon.SyphonServer");
+      syphonConst = syphonServerClass.getConstructor(PApplet.class, String.class);
+      stop = syphonServerClass.getDeclaredMethod("stop");
+      sendImage = syphonServerClass.getDeclaredMethod("sendImage", PGraphics.class);
+      syphonServer = syphonConst.newInstance(app, SYPHON_INSTANCE_NAME);
+      
+      System.out.println("SYPHON INITIALZIED");
+    }
+    catch (NoSuchMethodException | SecurityException | ClassNotFoundException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+    {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void cleanup()
+  {
+    if (syphonServer != null && stop != null)
+    {
+      try
+      {
+        stop.invoke(syphonServer);
+        System.out.println("CLEANUP");
+      }
+      catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+      {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @Override
 	public void send(PGraphics g) {
-		server.sendImage(g);	
+	  if(syphonServer != null && sendImage != null)
+	  {
+	    try
+      {
+        sendImage.invoke(syphonServer, g);
+        sendImage = null;
+      }
+      catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+      {
+        cleanup();
+        e.printStackTrace();
+      }
+	  }
 	}
-
 }
