2affb870578d88ed81f924c25e5ce546a5a4a20c
Daniel B. Chapman <chapman@danielbchapman.com>
2016-04-04 15:37:51 -0600

    - added some abstract emitters

--- src/main/java/com/danielbchapman/physics/toxiclibs/Emitter.java
+++ src/main/java/com/danielbchapman/physics/toxiclibs/Emitter.java
@@ -19,8 +19,7 @@ public abstract class Emitter<T extends Point>
   protected long nextDelta = -1L;
   @Getter
   @Setter
-  public 
-  VerletPhysics3D physics = Actions.engine.getPhysics();
+  public VerletPhysics3D physics = Actions.engine.getPhysics();
   
   public Emitter(Vec3D position, Vec3D heading, int lifeSpan, int rate, float randomVector, int randomTime)
   {
